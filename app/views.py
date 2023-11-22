36e507d61294e0eb2e2961e97a93a20b6ddd3461
thirumala reddy <thirumalareddy797@gmail.com>
2023-11-22 17:16:40 +0530

    athentications

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
 from app.models import *
 from app.forms import EmployeeForm
 from django.http import HttpResponseRedirect
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+
 def home(requests):
     return render(requests,"home.html")
 
@@ -39,46 +44,50 @@ def delete_employee(requests,id):
     data = Employee.objects.get(id=id) # we can get specific data of object
     data.delete()
     return HttpResponseRedirect("/list")
+class EmployeeSignup(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = "signup.html"
+    success_url = reverse_lazy('home')
 
-# ORM
-def list_employe(requests):
-    # READ ALL
-    # all data fetch from database
-    data_all = Employee.objects.all()
-    # count
-    data_all_count = Employee.objects.all().count()
-    # order by
-    data_order = Employee.objects.all().order_by('age')# ASC
-    data_order_dsc = Employee.objects.all().order_by('-age')# ASC
-    # index
-    data_second_index = Employee.objects.all()[2]
-
-    # filter
-    data = Employee.objects.filter(age__gt=20)
-    # gt ==>greter than
-    # lt ==>less than
-    #gte,lte
-
-    daa = Employee.objects.filter(first_name__startswith="p",age__gt=20)
-
-    # GET Specific
-    # get specific person==prakash
-    data = Employee.objects.get(first_name="prakash")
-    print(data)
-
-    # update specific object
-    data_up = Employee.objects.get(first_name="prakash")
-    data_up.second_name = "daram"
-    data_up.age=20
-    data_up.save()
-
-    # delete all
-    data = Employee.objects.all().delete() # delete all
-
-    # specific object delete
-    data = Employee.objects.get(id=2)
-    data.delete()
-
-    # Create
-    data=Employee.objects.create(name='thirumala',salary=1000,address='reddy vari palli',age=20)
-    data.save()
+# ORM: Object oriented Method
+# def list_employe(requests):
+#     # READ ALL
+#     # all data fetch from database
+#     data_all = Employee.objects.all()
+#     # count
+#     data_all_count = Employee.objects.all().count()
+#     # order by
+#     data_order = Employee.objects.all().order_by('age')# ASC
+#     data_order_dsc = Employee.objects.all().order_by('-age')# ASC
+#     # index
+#     data_second_index = Employee.objects.all()[2]
+#
+#     # filter
+#     data = Employee.objects.filter(age__gt=20)
+#     # gt ==>greater than
+#     # lt ==>less than
+#     #gte,lte
+#
+#     daa = Employee.objects.filter(first_name__startswith="p",age__gt=20)
+#
+#     # GET Specific
+#     # get specific person==prakash
+#     data = Employee.objects.get(first_name="prakash")
+#     print(data)
+#
+#     # update specific object
+#     data_up = Employee.objects.get(first_name="prakash")
+#     data_up.second_name = "daram"
+#     data_up.age=20
+#     data_up.save()
+#
+#     # delete all
+#     data = Employee.objects.all().delete() # delete all
+#
+#     # specific object delete
+#     data = Employee.objects.get(id=2)
+#     data.delete()
+#
+#     # Create
+#     data=Employee.objects.create(name='thirumala',salary=1000,address='reddy vari palli',age=20)
+#     data.save()
--- djangoproject5_2/urls.py
+++ djangoproject5_2/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from app.views import *
 
 urlpatterns = [
@@ -25,6 +25,8 @@ urlpatterns = [
     path('create',create_employee,name="create"),
     path('detail/<int:id>/', detailed_employee, name="detail"),
     path('update/<int:id>/', update_employee, name="update"),
-    path('delete/<int:id>/', delete_employee, name="delete")
+    path('delete/<int:id>/', delete_employee, name="delete"),
+    path('signup',EmployeeSignup.as_view(),name='signup'),
+    path('accounts/',include('django.contrib.auth.urls'),name='login')
 
 ]
